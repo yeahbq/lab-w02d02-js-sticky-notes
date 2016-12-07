c198752814e0ec80ef835c78ce1134031ac73a49
Barrett Quan <21barrett@gmail.com>
2016-12-06 17:39:42 -0800

    needs to add function to close sticky

--- dom.js
+++ dom.js
@@ -6,9 +6,14 @@ var totalNotes = document.querySelector("input");
 var addNote = function() {
   var main = document.querySelector('#main');
   var newDiv = document.createElement('div');
+  var span = document.createElement('span');
   newDiv.classList.add('card');
+  span.classList.add('exit');
   newDiv.textContent = "click to edit";
+  span.textContent = 'x';
   main.appendChild(newDiv).contentEditable = true;
+  newDiv.appendChild(span);
+
   updateTotal();
   }
 
@@ -18,6 +23,15 @@ var updateTotal = function() {
   totalNotes.value = document.querySelectorAll('.card').length
 }
 
+// button.addEventListener('click', #)
+//function to click x to close box
 
+// var exit = function() {
+//   if (span.classList.contains('exit') {
+//     span.parentNode.remove();
+//   }
+// }
+
+// span.addEventListener("click", exit);
 
 
